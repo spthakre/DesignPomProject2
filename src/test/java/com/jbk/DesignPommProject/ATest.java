0a339b54d9a8d459aec1d222e965301f4488cee4
spthakre <spthakre22@gmail.com>
2020-03-03 15:45:03 +0530

    commit DesignPomProject...

---
+++ src/test/java/com/jbk/DesignPommProject/ATest.java
@@ -0,0 +1,38 @@
+package com.jbk.DesignPommProject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ATest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ATest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ATest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
